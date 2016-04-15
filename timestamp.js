d0eedb726c3b88bfe709bdf32e586c1dfdd66151
Kelsey Nadig <kelsey.nadig@gmail.com>
2016-04-15 13:12:55 -0400

    favicon etc

--- timestamp.js
+++ timestamp.js
@@ -17,25 +17,27 @@ app.get('/', function(req, res) {
     }
   });
 });
-app.set('etag', false);
+//app.set('etag', false);
 app.get('/:timestring', function(request, response) {
    var url_time = request.params.timestring;
-   var result;
    var whole_url = request.originalUrl;
    console.log("param: "+url_time);
+   console.log("param type "+typeof url_time);
 
    if (moment.unix(url_time).isValid()) {
+       var result;
        var unix = moment.unix(url_time).unix();
        var natural = moment.unix(url_time).format("MMMM DD, YYYY");
-       result = '{"unix": '+unix+', "natural": '+natural+' }';
+       result = '{"unix": '+unix+', "natural": '+natural+'}';
    } else  if (moment(url_time.split('%20'), "MMMM DD YYYY").isValid()) {
+       var result;
        var nat_time = moment(url_time.split('%20'), "MMMM DD, YYYY");
        var unix = nat_time.unix();
        var natural = nat_time.format("MMMM DD, YYYY");
-       result = '{"unix": '+unix+', "natural": '+natural+' }';
+       result = '{"unix": '+unix+', "natural": '+natural+'}';
    };
-   console.log(result);
-   console.log(typeof result);
+   console.log("result is "+result);
+   console.log("type of result is "+typeof result);
    response.sendStatus(result);
 });
 
